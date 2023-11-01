d0eb656f0d1c6eefaac3c407869f3265746e286a
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-11-01 23:49:49 +0100

    FEAT: basic vue views added

---
+++ focus-frontend/src/views/LogIn.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="log-in-view">
+    <h1>Log In</h1>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'LogIn'
+  }
+</script>
