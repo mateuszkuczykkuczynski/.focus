573a1744ae1d7f3c4dee60fdd83764fad5dfab03
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-11-05 23:32:59 +0100

    FEAT: JWT final

--- focus-frontend/src/views/HomeView.vue
+++ focus-frontend/src/views/HomeView.vue
@@ -1,9 +1,20 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
     <TheWelcome />
+    <!-- Add a logout button -->
+    <button @click="logout">Logout</button>
   </main>
 </template>
+
+<script setup>
+import { useAuthStore } from '@/stores/authUser';
+import TheWelcome from '../components/TheWelcome.vue';
+
+// Use the auth store
+const authStore = useAuthStore();
+
+// Define the logout function
+const logout = () => {
+  authStore.logout();
+};
+</script>
