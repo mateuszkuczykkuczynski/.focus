69089eadf4e3264866d614ea0be393b245024fa8
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-11-10 23:51:58 +0100

    FEAT(draft): Axios interceptors added

--- focus-frontend/src/stores/authUser.js
+++ focus-frontend/src/stores/authUser.js
@@ -1,21 +1,12 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 import router from "@/router";
-import { getJwtCreateUrl } from '@/config/api';
-import jwtDecode from 'jwt-decode';
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     accessToken: localStorage.getItem('accessToken') || null,
     refreshToken: localStorage.getItem('refreshToken') || null,
   }),
-    getters: {
-    isAccessTokenValid: (state) => {
-      if (!state.accessToken) return false;
-      const { exp } = jwtDecode(state.accessToken);
-      return Date.now() < exp * 1000;
-    },
-  },
   actions: {
   setTokens(data) {
     localStorage.setItem('accessToken', data.access);
@@ -34,11 +25,10 @@ export const useAuthStore = defineStore('auth', {
 
   async login(username, password) {
     try {
-        const url = getJwtCreateUrl();
-        const response = await axios.post(url, {
-          username,
-          password,
-        });
+      const response = await axios.post('http://127.0.0.1:8000/api/v1/jwt/create', {
+        username,
+        password,
+      });
 
       const { access, refresh } = response.data;
         this.setTokens({ access, refresh });
@@ -56,14 +46,15 @@ export const useAuthStore = defineStore('auth', {
   },
 });
 
+// For later usage
 // Set up Axios interceptor
-axios.interceptors.request.use(async (config) => {
-  const authStore = useAuthStore();
-  if (authStore.accessToken && !authStore.isAccessTokenValid) {
-    const newAccessToken = await authStore.refreshToken();
-    config.headers['Authorization'] = `JWT ${newAccessToken}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+// axios.interceptors.request.use(async (config) => {
+//   const authStore = useAuthStore();
+//   if (authStore.accessToken && !authStore.isAccessTokenValid) {
+//     const newAccessToken = await authStore.refreshAccessToken();
+//     config.headers['Authorization'] = `JWT ${newAccessToken}`;
+//   }
+//   return config;
+// }, (error) => {
+//   return Promise.reject(error);
+// });
